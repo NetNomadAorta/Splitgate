f56c315b2f290bf182ca7d6441a4e0b30c9eb4c9
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2022-11-15 22:40:48 -0500

    Update Image_Saver.py

--- Image_Saver.py
+++ Image_Saver.py
@@ -1,6 +1,7 @@
 import time
 from PIL import ImageGrab
 import winsound
+import sys
 
 
 
@@ -13,12 +14,29 @@ time.sleep(3)
 
 winsound.Beep(frequency, duration)
 
-for i in range(10):
+# Start FPS timer
+fps_start_time = time.time()
+
+for i in range(100):
     screenshot = ImageGrab.grab()
     
     screenshot.save('./Images/Screenshots/image-{}.jpg'.format(i))
     
     time.sleep(0.5)
     winsound.Beep(frequency, 40)
+    
+    ten_scale = 5
+    
+    i += 1
+    if i % ten_scale == 0:
+        fps_end_time = time.time()
+        fps_time_lapsed = fps_end_time - fps_start_time
+        fps = round(ten_scale/fps_time_lapsed, 2)
+        
+        sys.stdout.write('\033[2K\033[1G')
+        print("  ", fps, "FPS -",
+              end="\r"
+              )
+        fps_start_time = time.time()
     
 winsound.Beep(frequency-100, duration)
